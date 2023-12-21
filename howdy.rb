bda8262f82faca6dc92b1a8efe390300feeda8df
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2023-12-21 15:01:53 +0000

    Tested activesupport methods on strings.

--- howdy.rb
+++ howdy.rb
@@ -13,3 +13,11 @@ pp 53.ordinalize   # => "53rd"
 pp 2009.ordinalize # => "2009th"
 pp -21.ordinalize  # => "-21st"
 pp -134.ordinalize # => "-134th"
+
+pp "table".pluralize     # => "tables"
+pp "ruby".pluralize      # => "rubies"
+pp "equipment".pluralize # => "equipment"
+
+pp "tables".singularize    # => "table"
+pp "rubies".singularize    # => "ruby"
+pp "equipment".singularize # => "equipment"
