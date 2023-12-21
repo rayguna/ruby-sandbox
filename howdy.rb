b1e86d272dc3f29502b17ed01cd439f330a18906
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2023-12-21 14:56:20 +0000

    Added activesupport to howdy.rb

--- howdy.rb
+++ howdy.rb
@@ -4,3 +4,12 @@ my_string = "Hello, world!"
 pp my_string
 
 require "./goodbye.rb"
+
+require "active_support/all"
+#Try active support
+pp 1.ordinalize    # => "1st"
+pp 2.ordinalize    # => "2nd"
+pp 53.ordinalize   # => "53rd"
+pp 2009.ordinalize # => "2009th"
+pp -21.ordinalize  # => "-21st"
+pp -134.ordinalize # => "-134th"
